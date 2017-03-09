acf66ce873d04b8ec40c14637bb27e1e7625f931
nj <nikolaj@loftjoer.dk>
2017-03-10 00:32:18 +0100

    Only create authorized_keys if variable is present
    Avoid overwriting file, if BORG_AUTHORIZED_KEYS is not present.
    
    By doing this, allows mounting authorized_keys into the container - making support for multiple keys or special configuration.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 dpkg-reconfigure openssh-server
 
-echo $BORG_AUTHORIZED_KEYS > /home/borg/.ssh/authorized_keys
-chown borg.borg /home/borg/.ssh/authorized_keys
+if [ ! -z ${BORG_AUTHORIZED_KEYS+x} ]; then
+    echo $BORG_AUTHORIZED_KEYS > /home/borg/.ssh/authorized_keys
+    chown borg.borg /home/borg/.ssh/authorized_keys
+fi
 
 exec /usr/sbin/sshd -D
