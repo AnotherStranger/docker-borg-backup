a409542285e0b463efee09c29cc9a2d3f7d00cda
Thilo-Alexander Ginkel <tg@tgbyte.de>
2017-10-17 01:23:21 +0200

    Permission fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,7 @@ fi
 if [ ! -z ${BORG_AUTHORIZED_KEYS+x} ]; then
     echo $BORG_AUTHORIZED_KEYS > /home/borg/.ssh/authorized_keys
     chown borg.borg /home/borg/.ssh/authorized_keys
+    chmod og-rwx /home/borg/.ssh/authorized_keys
 fi
 
 chown -R borg.borg /home/borg
