cfe4fe45cdd3df39d947e815224327cd9a6ece24
Thilo-Alexander Ginkel <tg@tgbyte.de>
2017-10-17 01:04:21 +0200

    Fix ownership

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,18 @@
 
 dpkg-reconfigure openssh-server
 
-if [ ! -z ${BORG_AUTHORIZED_KEYS+x} ]; then
-    echo $BORG_AUTHORIZED_KEYS > /home/borg/.ssh/authorized_keys
-    chown borg.borg /home/borg/.ssh/authorized_keys
-fi
-
 if [ -n "${BORG_UID}" ]; then
     usermod -u ${BORG_UID} borg
 fi
 
 if [ -n "${BORG_GID}" ]; then
-    groupmod -g ${BORG_GID} borg
+    groupmod -o -g ${BORG_GID} borg
     usermod -g ${BORG_GID} borg
+fi
+
+if [ ! -z ${BORG_AUTHORIZED_KEYS+x} ]; then
+    echo $BORG_AUTHORIZED_KEYS > /home/borg/.ssh/authorized_keys
+    chown borg.borg /home/borg/.ssh/authorized_keys
 fi
 
 exec /usr/sbin/sshd -D
