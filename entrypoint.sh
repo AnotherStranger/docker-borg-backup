e87f021e22d72abd76b102920392ac1fc43c3948
Thilo-Alexander Ginkel <tg@tgbyte.de>
2017-10-17 00:47:32 +0200

    Allow configurable uid/gid

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,13 @@ if [ ! -z ${BORG_AUTHORIZED_KEYS+x} ]; then
     chown borg.borg /home/borg/.ssh/authorized_keys
 fi
 
+if [ -n "${BORG_UID}" ]; then
+    usermod -u ${BORG_UID} borg
+fi
+
+if [ -n "${BORG_GID}" ]; then
+    groupmod -g ${BORG_GID} borg
+    usermod -g ${BORG_GID} borg
+fi
+
 exec /usr/sbin/sshd -D
