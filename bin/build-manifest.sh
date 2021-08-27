1667c6a04dbcc42a7f76a07eb6aa10e7d9688029
Thilo-Alexander Ginkel <tg@tgbyte.de>
2021-08-27 12:13:07 +0200

    Don't omit manifest build for vulnerability fixes

--- bin/build-manifest.sh
+++ bin/build-manifest.sh
@@ -6,7 +6,11 @@ BORG_VERSION=$(cat .version)
 IMAGE_VERSION="${BORG_VERSION}"
 export TAG="${IMAGE_VERSION}"
 
-if [ "$FORCE" != "1" ]; then
+if [ -e .trivy-vulnerable ]; then
+  VULNERABLE="1"
+fi
+
+if [ "$FORCE" != "1" ] && [ -z "$VULNERABLE" ]; then
   echo Exit if "tgbyte/borg-backup:${IMAGE_VERSION}" already exists
   check-tag.sh "tgbyte/borg-backup:${IMAGE_VERSION}" && exit 0
 fi
