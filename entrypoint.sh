035cf0e3ca3c7a407be9d7c3e16157b71c95cc00
AnotherStranger <6563442+AnotherStranger@users.noreply.github.com>
2023-03-10 23:25:46 +0100

    feat(logging): :sparkles: add further logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "Starting borgbackup-server...."
+echo "Environment:"
+echo "   BORG_UID                   = ${BORG_UID}"
+echo "   BORG_GID                   = ${BORG_GID}"
+echo "   BORG_SERVE_ADDITIONAL_ARGS = ${BORG_SERVE_ADDITIONAL_ARGS}"
+echo "   BORG_AUTHORIZED_KEYS       = ${BORG_AUTHORIZED_KEYS}"
+echo ""
+
 mkdir -p /var/lib/docker-borg/ssh >/dev/null 2>&1
 mkdir -p /home/borg/backups
 
@@ -24,6 +32,9 @@ if [ -n "${BORG_AUTHORIZED_KEYS+x}" ]; then
     echo -e "${BORG_AUTHORIZED_KEYS}" | sed  -e "s/^/command=\"borg serve ${BORG_SERVE_ADDITIONAL_ARGS} --restrict-to-path \/home\/borg\/backups\" /"  >/home/borg/.ssh/authorized_keys
     chown borg:borg /home/borg/.ssh/authorized_keys
     chmod og-rwx /home/borg/.ssh/authorized_keys
+
+    echo "Printing the contents of /home/borg/.ssh/authorized_keys:"
+    cat /home/borg/.ssh/authorized_keys
 fi
 
 chown -R borg:borg /home/borg
