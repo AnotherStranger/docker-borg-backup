3c43648e4cd4880d13a4ac374fbec4d2dd0edabc
André Büsgen <andre.buesgen@posteo.de>
2023-03-21 11:11:46 +0100

    feat(docker): :sparkles: use multi-stage docker image to build borgbackup using pip
    We migrate the image to an alpine based python image and installed borgbackup using pip.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-echo "Starting borgbackup-server...."
-echo "Environment:"
-echo "   BORG_UID                   = ${BORG_UID}"
-echo "   BORG_GID                   = ${BORG_GID}"
-echo "   BORG_SERVE_ADDITIONAL_ARGS = ${BORG_SERVE_ADDITIONAL_ARGS}"
+echo "################################################################################"
+echo "#                          STARTING BORGBACKUP-SERVER                          #"
+echo "################################################################################"
 echo ""
 
 mkdir -p /var/lib/docker-borg/ssh >/dev/null 2>&1
-mkdir -p /home/borg/backups
+mkdir -p /home/borg/backups >/dev/null 2>&1
 
 # Create a random password each startup, as only ssh-key auth is allowed
 BORG_PASSWORD=$(openssl passwd -5 "$(openssl rand -base64 128)")
 usermod -p "$BORG_PASSWORD" borg > /dev/null
 usermod -U borg > /dev/null
 
+# Generate SSH host-key, if not present
 if [ ! -f /var/lib/docker-borg/ssh/ssh_host_rsa_key ]; then
     echo "Creating SSH keys. To persist keys across container updates, mount a volume to /var/lib/docker-borg..."
     ssh-keygen -A
@@ -37,11 +36,28 @@ if [ -n "${BORG_AUTHORIZED_KEYS+x}" ]; then
     chown borg:borg /home/borg/.ssh/authorized_keys
     chmod og-rwx /home/borg/.ssh/authorized_keys
 
-    echo "Printing the contents of /home/borg/.ssh/authorized_keys:"
+    echo "################################################################################"
+    echo "#          PRINTING THE CONTENTS OF /HOME/BORG/.SSH/AUTHORIZED_KEYS:           #"
+    echo "################################################################################"
     cat /home/borg/.ssh/authorized_keys
+    echo "end of /home/borg/.ssh/authorized_keys"
+    echo ""
 fi
 
 chown -R borg:borg /home/borg
 chown -R borg:borg /home/borg/.ssh
+
+echo "################################################################################"
+echo "#                    BORGBACKUP SERVER STARTED SUCCESSFULLY                    #"
+echo "################################################################################"
+echo "Environment:"
+echo "   BORG_UID                   = ${BORG_UID}"
+echo "   BORG_GID                   = ${BORG_GID}"
+echo "   BORG_SERVE_ADDITIONAL_ARGS = ${BORG_SERVE_ADDITIONAL_ARGS}"
+echo "Borg Version: $(borg --version)"
+echo "Following borg repos are present:"
+ls -Al /home/borg/backups
+echo "Size of all backups combined: $(du -sh /home/borg/backups)"
+
 
 exec /usr/sbin/sshd -D -e
